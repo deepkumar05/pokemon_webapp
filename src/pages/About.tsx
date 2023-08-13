1392f40f5ff42de5400e5afd153eb400223637c8
deepkumar05 <115537119+deepkumar05@users.noreply.github.com>
2023-08-14 04:21:14 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,26 +1,29 @@
 import React from "react";
 import Wrapper from "../sections/Wrapper";
-import avatarImage from "../assets/kishan.jpeg";
-import { FaYoutube, FaGithub, FaLinkedin } from "react-icons/fa";
+import avatarImage from "../assets/deep.jpg";
+import { FaGithub, FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 function About() {
   return (
     <div className="profile">
       <img src={avatarImage} alt="" className="profile-image" />
-      <h1 className="profile-text">Hi I am kishan Sheth</h1>
-      <h2 className="profile-text">The creator of this awesome pokedex</h2>
+      <h1 className="profile-text">Hi, I am Deepanshu Kumar</h1>
+      <h2 className="profile-text">The creator of this awesome website.</h2>
       <h4 className="profile-text">
-        This project is created for youtube tutorial for my channel Kishan Sheth
+        You are welcome to explore a captivating Pokémon platform<br />that enables you to effortlessly compare different species and <br /> curate your very own list of favorites.
       </h4>
       <div className="profile-links">
-        <a href="https://github.com/koolkishan">
+        <a href="https://github.com/deepkumar05" target='_blank' rel="noreferrer">
           <FaGithub />
         </a>
-        <a href="https://www.youtube.com/c/KishanSheth21">
-          <FaYoutube />
+        <a href="https://www.instagram.com/deep_ds_14/" target='_blank' rel="noreferrer">
+          <FaInstagram />
         </a>
-        <a href="https://www.linkedin.com/in/koolkishan/">
+        <a href="https://www.linkedin.com/in/krdeepanshu/" target='_blank' rel="noreferrer">
           <FaLinkedin />
+        </a>
+        <a href="https://twitter.com/Deepans07930899" target='_blank' rel="noreferrer">
+          <FaTwitter />
         </a>
       </div>
     </div>
